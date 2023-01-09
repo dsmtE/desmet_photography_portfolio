fead9681f00db20355875c635139f9916c1048dd
dsmtE <desmet.enguerrand@gmail.com>
2023-01-09 22:49:33 +0100

    🎨 ✨ better gallery and photoswipe lightbox

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,14 @@
 <script>
-	import Gallery from 'svelte-gallery';
-	import { onMount } from "svelte";
-
+	import PhotoswipeGallery from 'svelte-photoswipe-gallery';
 	import { images } from '$src/images.json';
-
-	let innerWidth = 0
-    let innerHeight = 0
-	let galleryID = 'gallery'
 </script>
 
-<svelte:window bind:innerWidth bind:innerHeight />
-
 <section>
-	<Gallery {images} gutter={4} rowHeight={400} />
+	<PhotoswipeGallery
+	maxColumnWidth={400}
+	{images}
+	showDescription="{false}"
+	showDownloadButton="{false}"
+	showFullscreenButton="{true}"
+/>
 </section>
