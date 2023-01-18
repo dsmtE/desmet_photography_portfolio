64c57d6a3fc0d92d9ac17c886a3937225253bdad
dsmtE <desmet.enguerrand@gmail.com>
2023-01-18 20:56:54 +0100

    🚀 deploy update basePath

--- _app/immutable/chunks/0-c7e61d45.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_layout.svelte-0c95c9dd.js";export{t as component};
---
+++ internal/immutable/chunks/0-33801b9f.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_layout.svelte-49429f13.js";export{t as component};
